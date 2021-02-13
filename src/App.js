710ee116d486e95c078ec177a635dc71963bb0f3
unknown <chrisitne.brown@bhcc.edu>
2021-02-13 06:54:44 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Joke from './components/layout/pages/Jokes'
+import About from './components/layout/pages/About'
+import Joke2 from './components/layout/pages/Jokes2'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+    
+        <div className="container mx-auto">
+          <Route path="/" exact component={Joke} />
+          <Route path="/about" exact component={About} />
+          <Route path="/classComponet" exact component={Joke2} />
+
+        </div>
+      </div>
+    </Router>
+
+
   );
 }
 
